c1740fafdb267b57a85de1e936d26eb5d57073dd
elzashabani13-eng <elzashabani13@gmail.com>
2026-02-01 21:42:46 +0100

    Datebase.php

---
+++ Datebase.php
@@ -0,0 +1,16 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $user = "root";
+    private $pass = "";
+    private $dbname = "electrobee";
+    private $conn;
+
+    public function connect(){
+        $this->conn = new mysqli($this->host,$this->user,$this->pass,$this->dbname);
+        if($this->conn->connect_error){
+            die("DB ERROR: ".$this->conn->connect_error);
+        }
+        return $this->conn;
+    }
+}
